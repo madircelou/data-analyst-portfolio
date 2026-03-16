cb1f4a9337f142acbb00bc8d0bd833fa792ccdc8
madircelou <elo.marcouire@gmail.com>
2026-03-16 14:45:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,3 +25,27 @@ st.markdown("""
 
 </style>
 """, unsafe_allow_html=True)
+
+st.title("Elodie Marcouire")
+st.subheader("Data Analyst Portfolio")
+
+st.markdown('<div class="presentation">', unsafe_allow_html=True)
+
+col1, col2 = st.columns([1,2])
+
+with col1:
+    st.image("photo.jpg", width=200)
+
+with col2:
+    st.markdown("""
+### About me
+
+Data analyst en alternance, issue d’une formation en **Mathématiques & Informatique**.  
+Je travaille sur l’analyse de données, la création de dashboards et l’aide à la prise de décision.
+
+J’aime explorer les données pour comprendre des phénomènes réels et transformer les analyses en insights utiles.
+
+**Tools:** Python | SQL | Power BI | Excel
+""")
+
+st.markdown('</div>', unsafe_allow_html=True)
